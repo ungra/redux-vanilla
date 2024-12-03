2e70bbdddd236f4ffb1eb554c21bf74fd5c4b65c
ungra <ungrarhythm@naver.com>
2024-12-03 23:10:13 +0900

    store, reducer

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Learning Redux-VanillaJavaScript
+# Learning Redux-VanillaJavaScript
+
+store
+: state 저장소
+
+reducer
+: store는 reducer를 필요로 함.
+: reducer는 state를 update하는 function임.
+: return value가 application의 data임.
+
+redux는 data를 한곳으로 모아서 관리.
--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
+import { createStore } from "redux";
+
 const Add = document.getElementById("Add");
 const Minus = document.getElementById("Minus");
 const Number = document.querySelector("span");
 
-let count = 0;
-
-Number.innerText = count;
-
-const handleAdd = () => {
-  count = count + 1;
-  updateNumber();
+const countModifirer = (count = 0) => {
+  return count;
 };
 
-const handleMinus = () => {
-  count = count - 1;
-  updateNumber();
-};
-
-const updateNumber = () => {
-  Number.innerText = count;
-};
-
-Add.addEventListener("click", handleAdd);
-Minus.addEventListener("click", handleMinus);
+const countStore = createStore(countModifirer);
