95e80e903f31db6817716864863f911c07fa9272
ungra <ungrarhythm@naver.com>
2024-12-04 20:05:08 +0900

    connecting the store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./components/App";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
